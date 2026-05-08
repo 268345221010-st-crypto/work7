fef8f54d609933cf5b05d95488692247f33e494b
268345221010-st-crypto <268345221010-st@rmutsb.ac.th>
2026-05-08 15:18:38 +0700

    ggg

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
+// Dark Mode
+function toggleDarkMode() {
+
+  document.body.classList.toggle("bg-gray-900");
+  document.body.classList.toggle("text-white");
+
+}
+
+// Hamburger Menu
+function toggleMenu() {
+
+  document
+    .getElementById("mobileMenu")
+    .classList.toggle("hidden");
+
+}
+
+// Modal
 function openModal() {
-  document.getElementById("modal").classList.remove("hidden");
+
+  document
+    .getElementById("modal")
+    .classList.remove("hidden");
+
 }
 
 function closeModal() {
-  document.getElementById("modal").classList.add("hidden");
+
+  document
+    .getElementById("modal")
+    .classList.add("hidden");
+
+}
+
+// Toast
+function showToast() {
+
+  const toast =
+    document.getElementById("toast");
+
+  toast.classList.remove("hidden");
+
+  setTimeout(() => {
+
+    toast.classList.add("hidden");
+
+  }, 3000);
+
 }
